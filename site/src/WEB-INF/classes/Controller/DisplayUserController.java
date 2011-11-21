13481074104002930a3741f58e0cf1a59f5c71f8
Christiaan Fernando <chrisfod@gmail.com>
2011-11-20 17:28:34 -0800

    Minor changes to controllers

--- site/src/WEB-INF/classes/Controller/DisplayUserController.java
+++ site/src/WEB-INF/classes/Controller/DisplayUserController.java
@@ -6,7 +6,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Time;
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
--- site/src/WEB-INF/classes/Controller/RacerController.java
+++ site/src/WEB-INF/classes/Controller/RacerController.java
@@ -13,12 +13,16 @@ import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
 import Beans.*;
+
 /**
  * 
  * @author Masterfod
- *
+ * 
  */
 public class RacerController {
+	private final int CREATE_RACER = 1;
+	private final int ADD_POINTS = 2;
+
 	private int raceId;
 	private int userId;
 	private boolean attend;
@@ -26,9 +30,9 @@ public class RacerController {
 	private int place;
 	private int score;
 	private Racers racer;
-	
-	private String address = "";	//Change me
-	
+
+	private String address = ""; // Change me
+
 	/**
 	 * 
 	 * @param request
@@ -37,47 +41,51 @@ public class RacerController {
 	 * @throws IOException
 	 * @throws SQLException
 	 */
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		DateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+		int requestType = Integer
+				.parseInt(request.getParameter("request_type"));
+		RequestDispatcher dispatcher;
 		attend = true;
-		
+
 		raceId = Integer.parseInt(request.getParameter("raceId"));
 		userId = Integer.parseInt(request.getParameter("userId"));
-		
-		//This might not work
-		try {
-			totalTime = (Time) sdf.parse(request.getParameter("totalTime"));
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+		switch (requestType) {
+		case (CREATE_RACER):
+
+			// This might not work
+			try {
+				totalTime = (Time) sdf.parse(request.getParameter("totalTime"));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			place = Integer.parseInt(request.getParameter("place"));
+			score = Integer.parseInt(request.getParameter("score"));
+
+			racer = new Racers(raceId, userId, attend, totalTime, place);
+			racer.racersDB();
+
+			request.setAttribute("racer", racer); // SETS THE ITEM IN THE
+													// SESSION
+
+			dispatcher = request.getRequestDispatcher(address); // FORWARDS TO
+																// THE NEXT PAGE
+			dispatcher.forward(request, response);
+			break;
+
+		case (ADD_POINTS):
+			score = Integer.parseInt(request.getParameter("score"));
+
+			racer.racersScoreDB(raceId, userId, score);
+
+			dispatcher = request.getRequestDispatcher(address); // FORWARDS TO
+																// THE NEXT PAGE
+			dispatcher.forward(request, response);
+			break;
 		}
-		
-		place = Integer.parseInt(request.getParameter("place"));
-		score = Integer.parseInt(request.getParameter("score"));
-		
-		racer = new Racers(raceId, userId, attend, totalTime, place);
-		racer.racersDB();
-	
-		request.setAttribute("racer", racer); //SETS THE ITEM IN THE SESSION
-		
-		RequestDispatcher dispatcher = request.getRequestDispatcher(address); //FORWARDS TO THE NEXT PAGE
-		dispatcher.forward(request, response);
-	}
-	
-	/**
-	 * 
-	 * @param request
-	 * @param response
-	 * @throws ServletException
-	 * @throws IOException
-	 * @throws SQLException
-	 */
-	public void addPoints (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		raceId = Integer.parseInt(request.getParameter("raceId"));
-		userId = Integer.parseInt(request.getParameter("userId"));
-		score = Integer.parseInt(request.getParameter("score"));
-				  
-		racer.racersScoreDB(raceId, userId, score);
 	}
 }
