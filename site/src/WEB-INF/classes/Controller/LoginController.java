c385baaa7a543749db5d232661f14a701f35fc86
ignacio@ignacio.ca <ignacio@host65-223.wifi.ubc.ca>
2011-11-22 11:43:05 -0800

    Library Classes and Login Controller Skeleton

---
+++ site/src/WEB-INF/classes/Controller/LoginController.java
@@ -0,0 +1,21 @@
+package Controller;
+
+import Beans.*;
+
+public class LoginController {
+
+    
+	/**
+	 * 
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 * @throws SQLException
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException { 
+        
+	}
+    
+}   
