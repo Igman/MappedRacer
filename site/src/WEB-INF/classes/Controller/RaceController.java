59515c18629985f9170399653e3463110c6e10d2
Christiaan Fernando <chrisfod@gmail.com>
2011-11-26 19:29:06 -0800

    RaceController

--- site/src/WEB-INF/classes/Controller/RaceController.java
+++ site/src/WEB-INF/classes/Controller/RaceController.java
@@ -95,7 +95,6 @@ public class RaceController extends HttpServlet {
 	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		JSONObject items, racers;
 		// Parse values from the request to determine type of request.
 		// Switch statement that checks first parameter and acts accordingly
 		int requestType = Integer
@@ -104,21 +103,14 @@ public class RaceController extends HttpServlet {
 		case CREATE_RACE:
 			createRace(request, response);
 			break;
+			/*
 		case ADD_RACER:
-			addRacer(request, response);
+			addRacers(request, response);
 			break;
 		case ADD_ITEM:
-			addItem(request, response);
+			addItems(request, response);
 			break;
-		case END_RACE:
-			endRace(request, response);
-			break;
-		case CHECK_IN:
-			checkIn(request, response);
-			break;
-		case CHECK_RACE:
-			checkRace(request, response);
-			break;
+			*/
 		default:
 			error("Unknown request", request, response);
 		}
@@ -137,6 +129,7 @@ public class RaceController extends HttpServlet {
 	 */
 	private void createRace(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
+		JSONObject items, racers;
 		// Parses all of the parameters out of the request.
 		int userID = Integer.parseInt(request.getParameter("user_id"));
 		Date startTime;
@@ -152,10 +145,8 @@ public class RaceController extends HttpServlet {
 			return;
 		}
 		String endPoint = request.getParameter("end_point");
-		String name = request.getParameter("name");
+		String raceName = request.getParameter("name");
 		Date createDate = new Date();
-
-		JSONObject items, racers;
 
 		items = (JSONObject) JSONSerializer.toJSON(request
 				.getAttribute("items"));
@@ -166,13 +157,13 @@ public class RaceController extends HttpServlet {
 		addRacersHelper(racers);
 
 		// Calls the model to create the race and returns the race id
-		int raceID = race.createRace(name, endPoint, startTime, createDate,
+		int raceID = race.createRace(raceName, endPoint, startTime, createDate,
 				userID);
 
-		boolean successful = addRacers(racers, raceID);
-		boolean successful2 = addItems(items, raceID);
+		boolean raceSuccess = addRacersHelper(racers);
+		boolean itemSuccess = addItemsHelper(items);
 
-		if (raceID != -1 && successful) {
+		if (raceID != -1 && raceSuccess) {
 			// Determines the next page.
 			RequestDispatcher dispatcher = request
 					.getRequestDispatcher("/WEB-INF/JSPs/add_items.jsp");
@@ -185,152 +176,6 @@ public class RaceController extends HttpServlet {
 
 	}
 
-	private boolean addItems(ArrayList<Item> items) {
-		// Take list add it to the DB
-	}
-
-	private boolean addRacers(ArrayList items) {
-		// Take list add new racers to the db
-	}
-
-	// TODO if this is is done all on the same page how will the request work?
-	/**
-	 * This is where the race creator will add a new user to the race. It passes
-	 * along the responsibility to the RacerController.
-	 * 
-	 * @param request
-	 *            Request obj containing parameters passed and session returned.
-	 * @param response
-	 * @throws ServletException
-	 * @throws IOException
-	 */
-	private void addRacer(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		int raceID = Integer.parseInt(request.getParameter("race_id"));
-		int userID = Integer.parseInt(request.getParameter("user_id"));
-
-		// Calls racer controller to handle making the new racer.
-		boolean successful = racers.addRacer(raceID, userID);
-
-		if (successful) {
-			// Determines the next page.
-			RequestDispatcher dispatcher = request
-					.getRequestDispatcher("/WEB-INF/JSPs/race_created");
-			dispatcher.forward(request, response);
-		} else {
-			error("Racer could not be added.", request, response);
-		}
-	}
-
-	private void addItem(ArrayList items) {
-		int points = Integer.parseInt(request.getParameter("points"));
-		String location = request.getParameter("location");
-
-		boolean successful = race.addItem(points, location);
-
-	}
-
-	/**
-	 * This ends the race telling the DB that no further checkins will occur.
-	 * 
-	 * @param request
-	 *            Request obj containing parameters passed and session returned.
-	 * @param response
-	 * @throws ServletException
-	 * @throws IOException
-	 */
-	private void endRace(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		int raceID = Integer.parseInt(request.getParameter("race_id"));
-
-		// Sets the db to finished the race so players querying will
-		boolean successful = race.finishRace(raceID);
-		// TODO: what else happens when race finishes? we should calculate
-		// scores here for the database.
-
-		if (successful) {
-			// Determines the next page.
-			RequestDispatcher dispatcher = request
-					.getRequestDispatcher("/WEB-INF/JSPs/race_results.jsp");
-			dispatcher.forward(request, response);
-		} else {
-			error("Could not end the race.", request, response);
-		}
-	}
-
-	// TODO: Check in has to determine where it is and if it is at end or at
-	// item.
-	/**
-	 * Check in from a user in a race that will then figure out if the check in
-	 * is at the finish line or at a item. It also could include an image or
-	 * comment.
-	 * 
-	 * @param request
-	 *            Request obj containing parameters passed and session returned.
-	 * @param response
-	 * @throws ServletException
-	 * @throws IOException
-	 */
-	private void checkIn(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		int raceID = Integer.parseInt(request.getParameter("race_id"));
-		int userID = Integer.parseInt(request.getParameter("user_id"));
-		String picUrl = request.getParameter("pic_url");
-		String comment = request.getParameter("comment");
-		String location = request.getParameter("location");
-
-		// TODO: Not sure the relationship between the two controllers will be
-		// here.
-		// Checkins jobs will be to determine if the user was in a item or at
-		// the finish line.
-		// Sets the db to finished the race so players querying will
-		boolean successful = checkIn.userCheckIn(raceID, userID, picUrl,
-				comment, location);
-
-		if (successful) {
-			// Determines the next page.
-			RequestDispatcher dispatcher = request
-					.getRequestDispatcher("/WEB-INF/JSPs/map.jsp");
-			dispatcher.forward(request, response);
-		} else {
-			error("Unable to check in.", request, response);
-		}
-	}
-
-	/**
-	 * The user can check if the race has started.
-	 * 
-	 * @param request
-	 *            Request obj containing parameters passed and session returned.
-	 * @param response
-	 * @throws ServletException
-	 * @throws IOException
-	 */
-	private void checkRace(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
-		String raceID = request.getParameter("race_id");
-		String userID = request.getParameter("user_id");
-
-		boolean started = race.checkRace(raceID, userID);
-
-		request.setAttribute("started", started);
-
-		// Determines the next page.
-		RequestDispatcher dispatcher = request
-				.getRequestDispatcher("/WEB-INF/JSPs/race.jsp");
-		dispatcher.forward(request, response);
-	}
-
-	/**
-	 * This is how this controller will handle problems that occur with the DB
-	 * it will send a message back to UI and tell it to go to the error page.
-	 * 
-	 * @param request
-	 *            Request obj containing parameters passed and session returned.
-	 * @param response
-	 * @throws ServletException
-	 * @throws IOException
-	 */
 	private void error(String errMsg, HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
