9e98b1b6daf42b6eeb8c3b2f7bc73e3713c23d71
Ignacio Rodriguez <ignacio@ignacio.ca>
2011-12-05 23:27:17 -0800

    Authentication Filter

---
+++ site/src/Controller/AuthenticationFilter.java
@@ -0,0 +1,37 @@
+package Controller;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AuthenticationFilter implements Filter {
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		HttpSession session = httpRequest.getSession();
+		if(session.getAttribute("userid") == null){
+			httpResponse.sendRedirect("index.jsp");
+			return;
+		}
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+	}
+
+}
